83b1bd0d1c207d5d9e76ef5b5f3fa2f69a64423e
Jhzlo <105791673+JHZLO@users.noreply.github.com>
2024-08-09 17:36:20 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 121ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240809/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.java
+++ 240809/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.java
@@ -2,12 +2,16 @@ import java.util.Scanner;
 
 public class Main {
     public static boolean isSubSequence(int[] a, int[] b){
-
+        
         for(int i = 0; i < a.length - b.length; i++){
+            int isSub = 1;
             for (int j = i; j < i + b.length; j++){
                 if(a[j] != b[j-i]){
+                    isSub = 0;
                     continue;
                 }
+            }
+            if(isSub == 1){
                 return true;
             }
         }
