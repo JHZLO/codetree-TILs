4cc3009ed36f351f2b363b5c300870697ac904b9
Jhzlo <105791673+JHZLO@users.noreply.github.com>
2024-08-14 17:21:18 +0900

    Title: 반복 출력하기 2, Time: 162ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240814/반복 출력하기 2/repeated-output-2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void printHw(int n){
+        if(n==0){
+            return;
+        }
+        printHw(n-1);
+        System.out.println("HelloWorld");
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        printHw(n);
+    }
+}
