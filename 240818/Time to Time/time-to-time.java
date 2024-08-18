49396284c2c68ec1b0d9b5cb94cf9cec55560a91
Jhzlo <105791673+JHZLO@users.noreply.github.com>
2024-08-18 22:54:02 +0900

    Title: Time to Time, Time: 3011ms, Memory: 10MB, Status: Time Limit Exceed - CodeTree

---
+++ 240818/Time to Time/time-to-time.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int hour1 = sc.nextInt();
+        int minute1 = sc.nextInt();
+        int hour2 = sc.nextInt();
+        int minute2 = sc.nextInt();
+
+        int time = 0;
+
+        while(true){
+            if(hour1 == 4 && minute1 == 1){
+                break;
+            }
+
+            minute1 ++;
+            time ++;
+            if(minute1 == 60){
+                hour1 ++;
+                minute1 = 0;
+            }
+        }
+
+        System.out.print(time);
+
+        
+    }
+}
