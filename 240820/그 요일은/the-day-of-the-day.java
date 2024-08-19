68b24c30baa40ef968eba7f3a83c0a075e1a90b9
Jhzlo <105791673+JHZLO@users.noreply.github.com>
2024-08-20 00:30:15 +0900

    Title: 그 요일은, Time: 117ms, Memory: 10MB, Status: Passed - CodeTree

--- 240820/그 요일은/the-day-of-the-day.java
+++ 240820/그 요일은/the-day-of-the-day.java
@@ -1,52 +1,43 @@
 import java.util.Scanner;
 
 public class Main {
-    public static int sumOfDays(int[] days, int m, int d) {
-        int sum = 0;
-        for (int i = 0; i < m - 1; i++) {
-            sum += days[i];
-        }
-        sum += d;
-        return sum;
-    }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        int[] days = new int[]{31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}; // 윤년 고려
-        String[] weeks = new String[]{"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
-
         int m1 = sc.nextInt();
         int d1 = sc.nextInt();
         int m2 = sc.nextInt();
         int d2 = sc.nextInt();
-        String targetWeek = sc.next();
+        String day = sc.next();
 
-        int startDay = sumOfDays(days, m1, d1);
-        int endDay = sumOfDays(days, m2, d2);
+        String[] day_of_week = new String[]{"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+        int[] weekCheck = {0, 0, 0, 0, 0, 0, 0}; // 0 ~ 6 : 월 ~ 일
+        int[] days_of_month = new int[]{0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
-        // 시작 요일의 인덱스 계산
-        int startWeekIndex = 0;  // 월요일 인덱스는 0
-        int firstDayIndex = (startDay - 1 + startWeekIndex) % 7; 
+        int dayCheck = 0;
 
-        // 목표 요일의 인덱스
-        int targetIndex = -1;
-        for (int i = 0; i < 7; i++) {
-            if (targetWeek.equals(weeks[i])) {
-                targetIndex = i;
+        while (true){
+            if (m1 == m2 && d1 == d2){
+                weekCheck[dayCheck]++;
                 break;
             }
-        }
 
-        // 전체 차이에서 A 요일이 등장하는 횟수 계산
-        int totalDays = endDay - startDay + 1;
-        int count = 0;
-        for (int i = 0; i < totalDays; i++) {
-            if ((firstDayIndex + i) % 7 == targetIndex) {
-                count++;
+            d1++;
+            weekCheck[dayCheck++]++;
+
+            if (dayCheck > 6){
+                dayCheck = 0;
+            }
+
+            if (d1 > days_of_month[m1]){
+                m1++;
+                d1 = 1;
             }
         }
 
-        System.out.println(count);
+        for(int i = 0; i < 7; i++){
+            if (day_of_week[i].equals(day)){
+                System.out.println(weekCheck[i]);
+            }
+        }
     }
 }
