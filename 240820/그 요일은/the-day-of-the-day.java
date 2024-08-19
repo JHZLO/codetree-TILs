78b40256f6247ade9b4a1e9cd7549f3fdf72fb9e
Jhzlo <105791673+JHZLO@users.noreply.github.com>
2024-08-20 00:13:55 +0900

    Title: 그 요일은, Time: 115ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240820/그 요일은/the-day-of-the-day.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int sumOfDays(int[] days, int m, int d){
+        int sum = 0;
+
+        for(int i=0; i<m-1; i++){
+            sum += days[i];
+        }
+
+        sum += d;
+
+        return sum;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] days = new int[]{31,29,31,30,31,30,31,31,30,31,30,31};
+        String[] weeks = new String[]{"Mon","Tue","Wed","Thu","Fri","Sat","Sun"};
+
+        int m1 = sc.nextInt();
+        int d1 = sc.nextInt();
+        int m2 = sc.nextInt();
+        int d2 = sc.nextInt();
+
+        int startDay = sumOfDays(days, m1, d1);
+        int endDay = sumOfDays(days, m2, d2);
+
+        String w = sc.next();
+
+        int startWeekIndex = 0;  // 월요일 인덱스
+        int targetIndex = -1;
+
+        for (int i = 0; i < 7; i++) {
+            if (w.equals(weeks[i])) {
+                targetIndex = i;
+                break;
+            }
+        }
+
+        // 전체 차이에서 A 요일이 등장하는 횟수 계산
+        int totalDays = endDay - startDay;
+        int offset = (targetIndex - startWeekIndex + 7) % 7;
+
+        int day_offset = (d1 + offset) % 7;
+        int count = 1;
+
+        // System.out.println("totalDays:"+totalDays);
+        while(true){
+            // System.out.println("day_offset:"+day_offset);
+            day_offset += 7;
+            if(day_offset+d1%7 > totalDays){
+                break;
+            }
+            count += 1;
+        }
+
+        System.out.println(count);
+    }
+}
